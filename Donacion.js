25223ad6ebae10dcf2a1d72239d047e7951857d0
Carlos Colmenarez <carlos.ecn2010@gmail.com>
2023-05-12 17:41:48 -0400

    abrir galeria

---
+++ Donacion.js
@@ -0,0 +1,10 @@
+const FulImgBox = document.getElementById("fulImgBox"),
+fulimg = document.getElementById("fulImg");
+
+function openfulImg(){
+    FulImgBox.style.display ="flex"
+}
+
+function closeImg(){
+    FulImgBox.style.display = "none";
+}
